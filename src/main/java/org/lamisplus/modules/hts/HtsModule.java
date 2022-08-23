426086a7b2d6f9d1970271a04f7aabf996e01f29
aniwange33 <terteseamos@gmail.com>
2022-08-23 09:01:45 +0100

    feat: setup completed

---
+++ src/main/java/org/lamisplus/modules/hts/HtsModule.java
@@ -0,0 +1,29 @@
+package org.lamisplus.modules.hts;
+
+import com.foreach.across.config.AcrossApplication;
+import com.foreach.across.core.AcrossModule;
+import com.foreach.across.core.context.configurer.ComponentScanConfigurer;
+
+@AcrossApplication(
+        modules = {
+
+        }
+)
+public class HtsModule extends AcrossModule {
+    public static final String NAME = "HtsModule";
+
+    public HtsModule() {
+        super ();
+        addApplicationContextConfigurer (new ComponentScanConfigurer (
+                getClass ().getPackage ().getName () + ".repositories",
+                getClass ().getPackage ().getName () + ".service",
+                getClass ().getPackage ().getName () + ".controller"
+        ));
+    }
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+}

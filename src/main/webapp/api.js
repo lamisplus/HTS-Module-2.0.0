9ad65e961eebfd9a5f61ac5adaca30b9bfc6632b
Mathew Adegbite <mathewadegbite@gmail.com>
2022-10-27 19:07:34 +0100

    today with Emeka to fix merge code

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,4 +1,4 @@
-export const  token = (new URLSearchParams(window.location.search)).get("jwt")
-export const url = '/api/v1/'
-// export const url =  'http://localhost:8383/api/v1/';
-// export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjY2NTc4NzkzfQ.akWxvQInPcBkm8s_FujbBqzCIEBTdDfogwpkxNqmGYD4U_Y54LZbWZzn2mQdO9kAJ8x6Swduws0TvpYMODbhaA';
+// export const  token = (new URLSearchParams(window.location.search)).get("jwt")
+// export const url = '/api/v1/'
+export const url =  'http://localhost:8282/api/v1/';
+export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjY2OTI3MzQzfQ.6niJ363lrSIbYBE3H-xv3bBp7ejJ_I_KBcKjYGEn9ies0fqgZ28GXkKo5elVjt9WE54lKdjhIV8qgfeawkVUnw';

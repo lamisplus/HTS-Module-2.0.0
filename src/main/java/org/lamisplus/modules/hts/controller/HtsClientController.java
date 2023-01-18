f56a31f50522b82d6b25d19f7612feba9c4281cc
Emeka <ilozuechukwuemeka@yahoo.com>
2023-01-18 14:45:19 +0100

    update api for current hts

--- src/main/java/org/lamisplus/modules/hts/controller/HtsClientController.java
+++ src/main/java/org/lamisplus/modules/hts/controller/HtsClientController.java
@@ -2,16 +2,16 @@ package org.lamisplus.modules.hts.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.lamisplus.modules.base.domain.dto.PageDTO;
+import org.lamisplus.modules.base.util.PaginationUtil;
 import org.lamisplus.modules.hts.domain.dto.*;
+import org.lamisplus.modules.hts.domain.entity.HtsPerson;
 import org.lamisplus.modules.hts.service.HtsClientService;
 import org.lamisplus.modules.hts.service.IndexElicitationService;
 import org.lamisplus.modules.patient.domain.entity.Person;
 import org.springframework.data.domain.Page;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -21,6 +21,7 @@ import java.util.List;
 public class HtsClientController {
     private final HtsClientService htsClientService;
     private final String HTS_URL_VERSION_ONE = "/api/v1/hts";
+    private final String HTS_URL_VERSION_TWO = "/api/v2/hts";
 
     private final IndexElicitationService indexElicitationService;
 
@@ -54,10 +55,10 @@ public class HtsClientController {
     public ResponseEntity<HtsClientDto> updateIndexNotificationServicesElicitation(@PathVariable Long id, @Valid @RequestBody IndexElicitationDto indexElicitationDto) {
         return ResponseEntity.ok(this.htsClientService.updateIndexNotificationServicesElicitation(id, indexElicitationDto));
     }*/
-    @GetMapping(HTS_URL_VERSION_ONE + "/{id}")
+    /*@GetMapping(HTS_URL_VERSION_ONE + "/{id}")
     public ResponseEntity<HtsClientDtos> getHtsClientById(@PathVariable Long id) {
         return ResponseEntity.ok(this.htsClientService.getHtsClientById(id));
-    }
+    }*/
     @GetMapping(HTS_URL_VERSION_ONE + "/{id}/index-elicitation")
     public ResponseEntity<List<IndexElicitationResponseDto>> getAllByHtsClientId(@PathVariable Long id) {
         return ResponseEntity.ok(this.indexElicitationService.getAllByHtsClientId(id));
@@ -66,6 +67,11 @@ public class HtsClientController {
     public ResponseEntity<HtsClientDtos> getHtsClientByPersonId(@PathVariable Long personId) {
         return ResponseEntity.ok(this.htsClientService.getHtsClientByPersonId(personId));
     }
+    @GetMapping(HTS_URL_VERSION_ONE + "/persons/{personId}/current-hts")
+    public ResponseEntity<HtsClientDto> getLatestHtsByPersonId(@PathVariable Long personId) {
+        return ResponseEntity.ok(this.htsClientService.getLatestHtsByPersonId(personId));
+    }
+
     @GetMapping(HTS_URL_VERSION_ONE + "/code/client-code")
     public ResponseEntity<String> getGenerateHtsClientCode() {
         return ResponseEntity.ok(this.htsClientService.getGenerateHtsClientCode());
@@ -74,7 +80,6 @@ public class HtsClientController {
     public ResponseEntity<String> getClientNameByCode(@PathVariable String code) {
         return ResponseEntity.ok(this.htsClientService.getClientNameByCode(code));
     }
-
     @GetMapping(HTS_URL_VERSION_ONE)
     public ResponseEntity<PageDTO> getHtsClients(@RequestParam (required = false, defaultValue = "*")  String searchValue,
                                                  @RequestParam (required = false, defaultValue = "20")int pageSize,
@@ -83,12 +88,17 @@ public class HtsClientController {
         //HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(ServletUriComponentsBuilder.fromCurrentRequest(), page);
         return new ResponseEntity<>(this.htsClientService.getAllHtsClientDTOSByPerson(page), HttpStatus.OK);
     }
-
+    @GetMapping(HTS_URL_VERSION_TWO + "/persons")
+    public ResponseEntity<PageDTO> getHtsPerson(@RequestParam (required = false, defaultValue = "*")  String searchValue,
+                                                 @RequestParam (required = false, defaultValue = "20")int pageSize,
+                                                 @RequestParam (required = false, defaultValue = "0") int pageNo) {
+        Page<HtsPerson> page = htsClientService.getAllPersonHts(searchValue, pageNo, pageSize);
+        return new ResponseEntity<>(PaginationUtil.generatePagination(page, page.getContent()), HttpStatus.OK);
+    }
     @GetMapping(HTS_URL_VERSION_ONE+ "/risk-stratification/person/{personId}")
     public ResponseEntity<HtsClientDtos> getRiskStratificationHtsClients(@PathVariable Long personId) {
         return ResponseEntity.ok(htsClientService.getRiskStratificationHtsClients(personId));
     }
-
     @GetMapping(HTS_URL_VERSION_ONE + "/persons")
     public ResponseEntity<PageDTO> getAllPerson(@RequestParam (required = false, defaultValue = "*")  String searchValue,
                                                 @RequestParam (required = false, defaultValue = "20")int pageSize,
@@ -97,7 +107,6 @@ public class HtsClientController {
                 .getAllHtsClientDTOSByPerson(htsClientService
                         .findHtsClientPersonPage(searchValue, pageNo, pageSize)), HttpStatus.OK);
     }
-
     @DeleteMapping(HTS_URL_VERSION_ONE + "/{id}")
     public void delete(@PathVariable Long id) {
         this.htsClientService.delete(id);

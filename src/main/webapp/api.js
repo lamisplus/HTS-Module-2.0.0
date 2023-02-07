9ce3665ba444db785079292cd9a34f2b55fe1083
vixxjy <ajorvictor48@gmail.com>
2023-02-07 12:47:51 +0100

    [HTS] update for hts fixes done

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,4 +1,4 @@
-export const  token = (new URLSearchParams(window.location.search)).get("jwt")
-export const url = '/api/v1/'
-//export const url =  'http://localhost:8383/api/v1/';
-//export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjc1ODAxNzE4fQ.Oy-CReprNoqT0QUrZ-fH0E2evyIOcTpBha0ZkogUJS2VIVcorf4Yg4d6SyvBiObRTFBmXcm4mGBSi02B4inQlA';
+//export const  token = (new URLSearchParams(window.location.search)).get("jwt")
+//export const url = '/api/v1/'
+export const url =  'http://localhost:8383/api/v1/';
+export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjc1ODAxNzE4fQ.Oy-CReprNoqT0QUrZ-fH0E2evyIOcTpBha0ZkogUJS2VIVcorf4Yg4d6SyvBiObRTFBmXcm4mGBSi02B4inQlA';

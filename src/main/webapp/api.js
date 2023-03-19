c519e7a6eb285a094c19bc4b8c487a1c692b1b3b
vixxjy <ajorvictor48@gmail.com>
2023-03-19 14:12:10 +0100

    [HTS] updated for patient status

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,5 +1,4 @@
 export const token = new URLSearchParams(window.location.search).get("jwt");
 export const url = "/api/v1/";
 //export const url = "http://localhost:8283/api/v1/";
-//export const token =
-("eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjc5MjY2NjE1fQ.9FyARXgDdTFOY3o5uFSol-ye6BtNtRdZ3NmWJe4soYprqjlFou_pf418GwZ0-jERU0CzHYtvR8b2oeC4WGZKPg");
+//export const token = ("eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjc5MjY2NjE1fQ.9FyARXgDdTFOY3o5uFSol-ye6BtNtRdZ3NmWJe4soYprqjlFou_pf418GwZ0-jERU0CzHYtvR8b2oeC4WGZKPg");

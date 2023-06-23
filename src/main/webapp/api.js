271a1c9ab462c2f7e0401bfe5b1131d3bb4d2191
Mathew Adegbite <mathewadegbite@gmail.com>
2023-06-23 10:31:37 +0100

    [HTS] Implementation of HTS clients list

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,7 +1,8 @@
+
 export const token = new URLSearchParams(window.location.search).get("jwt");
 export const url = "/api/v1/";
 /*
-export const url = "http://localhost:9090/api/v1/";
+export const url = "http://localhost:8080/api/v1/";
 export const token =
-  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjg2ODYwOTY5fQ.BM53rlVuhzvXXNCUTucJfEKfbzhJ1dwVYmlZ-KCQ9zFMyRuq52LTfyxcJyQEIXp3vchxbYGNMoyaF_vRwHEN1w";
+  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjg3NTU0MTMwfQ.SIJqehnCYBcjVwCZHq_CRp30pgZsvFEaRJgQifqn9k5THBfkOsAi_Yzc_QC0HPq7yzYMcG9jcnomeMhJo9zpjg";
 */

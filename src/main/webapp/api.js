4a8e14a1550044c23da23c0e7de0fb3c7ed43b70
vixxjy <ajorvictor48@gmail.com>
2023-07-05 13:23:59 +0100

    [HTS] chainset updated

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -4,5 +4,5 @@ export const url = "/api/v1/";
 /*
 export const url = "http://localhost:9090/api/v1/";
 export const token =
-  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjg3ODU5NjA0fQ.jL5P7kAy1QMyU-2yhHY8dwhyoQpmXTqdyx8My6ibHOVu4BQHXgqKdXckcfyevCCOPh8HPuGyMBO4MB-J0wq4vQ";
+  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjg3OTM3MjYxfQ.nuYrhZGw4YQsMNiivx5IW4rpRO-OBowj-Ah1g1KHE_9vC0TpUn_8ICHOlMT-Gf3mfWbe4CsbYF4dv1zGVAo1MQ";
 */

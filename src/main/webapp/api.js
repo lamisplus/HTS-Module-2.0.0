0c53e9ad9938ad06c427c6f38c9eafced11ebce2
danmanu44 <53683648+Danmanu44@users.noreply.github.com>
2024-02-27 19:25:01 +0100

    Viral load result date received

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,6 +1,6 @@
 
-// export const token = new URLSearchParams(window.location.search).get("jwt");
-// export const url = "/api/v1/";
+export const token = new URLSearchParams(window.location.search).get("jwt");
+export const url = "/api/v1/";
 
-export const url = "http://localhost:8383/api/v1/";
-export const token ="eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzA5MDg0NDM4fQ.Oa2WUbakPUqRKtnz8QQVCumj_nR_crQOlQCOWuf6_cQ7xI2CCoKY3GPVTi0nk69XKyGVANl322AW9oM-qbOhyA"
+// export const url = "http://localhost:8383/api/v1/";
+// export const token ="eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzA5MDg0NDM4fQ.Oa2WUbakPUqRKtnz8QQVCumj_nR_crQOlQCOWuf6_cQ7xI2CCoKY3GPVTi0nk69XKyGVANl322AW9oM-qbOhyA"

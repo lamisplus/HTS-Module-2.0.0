8d4c6f7bd2e8ae8fe4acb692bf8afe47d8ea74f9
Tech teni <yakubganiyat96@gmail.com>
2024-04-22 07:45:45 +0100

    restructured the payload

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,6 +1,6 @@
-// export const token = new URLSearchParams(window.location.search).get("jwt");
-// export const url = "/api/v1/";
+export const token = new URLSearchParams(window.location.search).get("jwt");
+export const url = "/api/v1/";
 
-export const url = "http://localhost:8383/api/v1/";
-export const token =
-  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzEzNzUxODgyfQ.IkfVyxPSpEVPMx4PP-bIqb423dEMWg_nfeFzin2VthOLymJa25ALbV_d43zSXfkfUvoFgTJdPW29KUtGXXPPaQ";
+// export const url = "http://localhost:8383/api/v1/";
+// export const token =
+//   "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzEzNzUxODgyfQ.IkfVyxPSpEVPMx4PP-bIqb423dEMWg_nfeFzin2VthOLymJa25ALbV_d43zSXfkfUvoFgTJdPW29KUtGXXPPaQ";

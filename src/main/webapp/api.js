9981537cafcda971aed898dff68b3f6b7b9df7d5
Tech teni <yakubganiyat96@gmail.com>
2024-04-30 11:58:17 +0100

    changed the api.s ort number

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,6 +1,6 @@
-export const token = new URLSearchParams(window.location.search).get("jwt");
-export const url = "/api/v1/";
+// export const token = new URLSearchParams(window.location.search).get("jwt");
+// export const url = "/api/v1/";
 
-// export const url = "http://localhost:8789/api/v1/";
-// export const token =
-// "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzE0NDYyNzYwfQ.vdaLAF8-slwbgTWDKz7QJAYnYoWagSHvWJUacwENX_i_vFihXZG-_mFiGrqqkJafSZ2TaJVVmEHs33NzKT7RfA"
+export const url = "http://localhost:8383/api/v1/";
+export const token =
+  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzE0NDkzMjUzfQ.RI2gtVKkE-RII-lxC1bGblWsKOpbV92dMztRhSH3dWBol7L6IsVn13WBO9GsfQJV4LAlXie66NVJnYPbuGVevw";

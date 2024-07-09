868547fe80ac86e8774891487c2ade8ab694bea5
Tech teni <yakubganiyat96@gmail.com>
2024-07-09 11:59:58 +0100

    rearranged the schem.xml

--- src/main/java/org/lamisplus/modules/hts/installers/SchemaInstaller.java
+++ src/main/java/org/lamisplus/modules/hts/installers/SchemaInstaller.java
@@ -7,7 +7,7 @@ import org.springframework.core.annotation.Order;
 @Order(1)
 @Installer(name = "schema-installer",
         description = "Installs the required database for hts tables",
-        version = 11)
+        version = 12)
 public class SchemaInstaller extends AcrossLiquibaseInstaller {
     public SchemaInstaller() {
         super("classpath:installers/hts/schema.xml");

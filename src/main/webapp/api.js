58163f4bb7a20828710348456bab3d9376bfbaa3
Tech teni <yakubganiyat96@gmail.com>
2024-07-29 11:43:57 +0100

    pulled the permission into family index

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -4,5 +4,5 @@ export const url =
     : "/api/v1/";
 export const token =
   process.env.NODE_ENV === "development"
-    ? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzIyMjY5NzY4fQ.NGUN_wW-Ndysswo_q_xfpQCMmw2sC_IZGW3aRsZ3pd0rZL1W-PdmSnJq0cK7T_-h6gtRVuZ3RvAeGRv4PtOBpQ"
+    ? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzIyMjc4MzU4fQ.-qY3WPGHyrwZP4593p4isv7-DUfV2zIu391UTDdiX0q46MAsCXN3VhIJkmK90iw1wwtNwjzVUHl137yIkcoDzg"
     : new URLSearchParams(window.location.search).get("jwt");

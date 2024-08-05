3c17fcacefadc18d224f17f1d0e69a1c5c055558
Tech teni <yakubganiyat96@gmail.com>
2024-08-05 12:01:46 +0100

    switching to family index teating

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -4,5 +4,5 @@ export const url =
     : "/api/v1/";
 export const token =
   process.env.NODE_ENV === "development"
-    ? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzIyODA5MDQ3fQ.DLUYf-oHdXp1-bkiTUefOAUbQfhuLrGZnghuKBDmdzLvufMmnVAIYUvydctElfFXFGCP-YwVALWJKo1hcKWTeg"
+    ? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzIyODc3NTIxfQ.PEmL7eNxdRz5DZ2jgLv7nBfrVRHpUFlVaNQtVgGpX8KHvY1Vgy404VClfAWxU-p-E8dnrh11R5gIBgdmXpJe9g"
     : new URLSearchParams(window.location.search).get("jwt");

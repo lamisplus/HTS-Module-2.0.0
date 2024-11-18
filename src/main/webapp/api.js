9ea2accec425af320e909ba4139645a2b21bf9ed
Yakub <ganiyat.yakub@thepalladiumgroup.com>
2024-11-18 12:13:52 +0100

    switching to family branch

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -4,7 +4,7 @@ export const url =
     : "/api/v1/";
 export const token =
   process.env.NODE_ENV === "development"
-    ? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzMxNjEwNTU0fQ.wwCgAkYVRpOSLaCH0dN3gBvV0G8rANidd3IQ6Oa_1tlGvm7KuYeZYmbjSFyZN_IgGmOAez0a7Guj1k5oZDHIRg"
+    ? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzMxOTU0NjcxfQ.z7UWr9LpbIVFdrCRCRWQ_TyBT4Hvy542G_xlz3GfGw7LXOp5ELtNFKSXkaDoz4AezIKNYXed1ETyCUTQ-sPXKw"
     : new URLSearchParams(window.location.search).get("jwt");
 
 let props = {
